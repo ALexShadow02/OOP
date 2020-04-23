f52cae504b9354caa2861d5f1b2cd80455fc043f
ALexShadow02 <54858128+ALexShadow02@users.noreply.github.com>
2020-04-23 11:19:12 +0300

    Add files via upload

---
+++ Lab6cpp.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+class Expression final {
+public:
+	double a, c, d;
+	Expression(double _a, double _c, double _d) {
+		a = _a;
+		c = _c;
+		d = _d;
+	}
+	double GetValue() {
+		if (a >= 4 || a == 0) throw - 1;
+		return (2 * c - d / 23) / (log(1 - a / 4));
+	}
+};
+int main()
+{
+	srand(time(0));
+	try {
+		Expression exp(rand()%5+1,rand()%10+1,rand()%10+1);
+		cout<<exp.GetValue()<<endl;
+    }
+	catch (int err) {
+		cout << "Invalid operand 'a' in the expression" << endl;
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace Lab6cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random(DateTime.Now.Millisecond);
+            Expression[] exps = new Expression[4];
+            for(int i = 0; i < 4; i++)
+            {
+                exps[i] = new Expression(rand.Next(1, 5), rand.Next(1, 100), rand.Next(1, 100));
+                try
+                {
+                    Write($"Значение {i + 1}-го выражения:");
+                    WriteLine(exps[i].Value);
+                }
+                catch (Exception ex)
+                {
+                    WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+    sealed class Expression
+    {
+        public double a { get; set; }
+        public double c { get; set; }
+        public double d { get; set; }
+        public double Value{get=>Calculate();}
+        public Expression(double _a,double _c,double _d)
+        {
+            a = _a;
+            c = _c;
+            d = _d;
+        }
+        private double Calculate()
+        {
+                if (a >= 4 || a == 0) throw new Exception("Недопустимый операнд 'a' в выражении");
+                return (2 * c - d / 23) / (Math.Log(1 - a / 4));
+        } 
+    }
+
+}
